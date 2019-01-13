9186a6ed2af753329c279784d004dc894dc37e71
asadalarma <asadalarma@gmail.com>
2019-01-14 01:12:19 +0500

    deleted files and add one file

---
+++ admin/database/foodorderingsystem.sql
@@ -0,0 +1,105 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : foodorderingsystem
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2019-01-14 01:10:33
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for deals
+-- ----------------------------
+DROP TABLE IF EXISTS `deals`;
+CREATE TABLE `deals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of deals
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for meals
+-- ----------------------------
+DROP TABLE IF EXISTS `meals`;
+CREATE TABLE `meals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `restaurant_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `price` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` varchar(255) DEFAULT NULL,
+  `updated_at` varchar(255) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of meals
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for meal_deals
+-- ----------------------------
+DROP TABLE IF EXISTS `meal_deals`;
+CREATE TABLE `meal_deals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `meal_id` int(11) NOT NULL,
+  `deal_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of meal_deals
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `user_type` enum('admin','customer','restaurant') NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `openhours` datetime DEFAULT NULL,
+  `closinghours` datetime DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'admin', 'admin@example.com', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin user', null, null, null, null, null, null, null, null, null, null, '0');
--- admin/database/users.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `users` VALUES (1, 'admin', 'admin@example.com', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin user', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
