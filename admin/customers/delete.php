6a73e89d36ff8bc198785f468acc2def1792606c
Muhammad Asad <muhammmad.asad@arpatech.com>
2019-01-29 10:15:54 +0500

    add delete page in customers

---
+++ admin/customers/delete.php
@@ -0,0 +1,19 @@
+<?php
+include("../requiredFiles.php");
+
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+    $updatequery = "update users set is_deleted=1 where id=$id";
+    $updatequery_result = mysqli_query($conn, $updatequery);
+    $count = mysqli_affected_rows($conn);
+    if($count){
+        $_SESSION["message"] = "Updated Successfully....!";
+        header('Location:index.php');
+    }else
+    {
+        $_SESSION["error"] = "Something Wrong....!";
+        header('Location:index.php');
+    }
+}
+
+
